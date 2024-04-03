d926c8348000ec89d390b890dc6ce07654771f7e
twdent <timo.denton@gmail.com>
2024-04-03 13:55:17 +0200

    Temp fix for python wrapper

--- elevation_mapping_cupy/script/elevation_mapping_cupy/elevation_mapping_ros.py
+++ elevation_mapping_cupy/script/elevation_mapping_cupy/elevation_mapping_ros.py
@@ -216,11 +216,11 @@ class ElevationMapWrapper:
 
         # assert np.all(np.array(camera_info_msg.D) == 0.0), "Undistortion not implemented"
         K = np.array(camera_info_msg.K, dtype=np.float32).reshape(3, 3)
-        # process pointcloud
-        # if sub_key == 'traversable_segmentation_probs':
-        #     channels = ["class_probabilities"]
-        # else:
-        channels = ["rgb"]
+        # process 
+        if sub_key == 'traversable_segmentation_probs':
+            channels = ["trav","non-trav", "null"]
+        else:
+            channels = ["rgb"]
         self._map.input_image(semantic_img, channels, R, t, K, camera_info_msg.height, camera_info_msg.width) # removed sub_key
 
         self._image_process_counter += 1
